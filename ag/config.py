9555f963707acca900a3f6dba40bfde796f0b135
mtx <m@nekoarch.cc>
2025-07-18 14:21:08 +0800

    [~]

--- ag/config.py
+++ ag/config.py
@@ -4,5 +4,5 @@ import os
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-BASE_URL = os.getenv("BASE_URL", "https://api.openai.com/")
+BASE_URL = os.getenv("BASE_URL", "https://api.openai.com")
 DEFAULT_MODEL = os.getenv("DEFAULT_MODEL", "gpt-4o")
